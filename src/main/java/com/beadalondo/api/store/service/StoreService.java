254bc2142deb2529aaf3cac3034d24eab0c37859
AKASY16 <akaseoyeon16@gmail.com>
2026-05-17 05:34:06 +0900

    - application.yaml와 application-secret.yaml 분리
    - StoreService.java의 Store 가져오는 기능을 첫 번째 Store 가져오는 것에서 랜덤 Store 가져오기로 변경

--- src/main/java/com/beadalondo/api/store/service/StoreService.java
+++ src/main/java/com/beadalondo/api/store/service/StoreService.java
@@ -6,6 +6,9 @@ import com.beadalondo.api.store.repository.StoreRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 @Transactional
 public class StoreService {
@@ -44,13 +47,18 @@ public class StoreService {
     }
 
     @Transactional(readOnly = true)
-    public Store getCurrentStore(){
-        //TODO: 추후, 로그인 한 사람의 가게 정보를 반환해주는 메서드로 변경
+    public Store getCurrentStore() {
+        // TODO: 추후, 로그인 한 사람의 가게 정보를 반환해주는 메서드로 변경
+        //  현재는 테스트용으로 DB에 등록된 가게 중 랜덤 1개 반환
+        List<Store> stores = storeRepository.findAll();
 
-        return storeRepository.findAll()
-                .stream()
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException("등록된 가게가 없습니다."));
+        if (stores.isEmpty()) {
+            throw new IllegalStateException("등록된 가게가 없습니다.");
+        }
+
+        int randomIndex = ThreadLocalRandom.current().nextInt(stores.size());
+
+        return stores.get(randomIndex);
     }
 
 }
